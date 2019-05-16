5d06351fdd43a5730f15b2e844467818b04046b3
Aziz Ali <aziz@ilovecoding.org>
2019-05-16 01:06:39 -0700

    Final React Population Solution

---
+++ src/App/index.js
@@ -0,0 +1,92 @@
+import React from 'react';
+
+import Year from '../Year';
+import Countries from '../Countries';
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      year: 2017,
+      country: '',
+      people: 0
+    };
+    this.changeInputs = this.changeInputs.bind(this);
+    this.getPopulation = this.getPopulation.bind(this);
+  }
+
+  changeInputs({ year = this.state.year, country = this.state.country }) {
+    this.setState(
+      {
+        year,
+        country
+      },
+      () => {
+        this.getPopulation();
+      }
+    );
+  }
+
+  getPopulation() {
+    const { year, country } = this.state;
+    if (!year || !country) return;
+
+    const url = `http://54.72.28.201/1.0/population/${year}/${country}/`;
+
+    this.setState(
+      {
+        isLoading: true
+      },
+      () => {
+        fetch(url, {
+          headers: {
+            accept: 'application/json; charset=utf=8'
+          }
+        })
+          .then((res) => res.json())
+          .then((json) => {
+            this.setState({
+              people: json.reduce((total, item) => {
+                return total + item.total;
+              }, 0),
+              isLoading: false
+            });
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+      }
+    );
+  }
+  render() {
+    const { year, people, isLoading } = this.state;
+    return (
+      <div className="container mt-5 mx-auto">
+        <div className="bg-white border border-dark p-5 rounded">
+          <h1 className="text-center">Population:</h1>
+          <form className="row">
+            <Countries
+              changeCb={(country) => {
+                this.changeInputs({ country });
+              }}
+            />
+            <Year
+              selected={year}
+              changeCb={(e) => {
+                this.changeInputs({ year: e.target.value });
+              }}
+            />
+          </form>
+          <div className="alert text-center h2">
+            {isLoading ? (
+              'Loading...'
+            ) : (
+                `${people.toLocaleString(navigator.language, { minimumFractionDigits: 0 })}`
+              )}
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
---
+++ src/Countries/index.js
@@ -0,0 +1,52 @@
+import React from 'react';
+
+export default class Countries extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      countries: []
+    };
+
+    this.changeCountry = this.changeCountry.bind(this);
+  }
+  componentWillMount() {
+    const url = 'http://54.72.28.201/1.0/countries';
+    fetch(url, {
+      headers: {
+        accept: 'application/json; charset=utf=8'
+      }
+    })
+      .then((res) => res.json())
+      .then((json) => {
+        this.setState({
+          countries: ['', ...json.countries]
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+  changeCountry(e) {
+    this.setState({
+      selected: e.target.value
+    });
+    this.props.changeCb(e.target.value);
+  }
+  render() {
+    const { countries, selected } = this.state;
+    return (
+      <div className="col">
+        <label htmlFor="country">Select Country</label>
+        <select value={selected} onChange={this.changeCountry} className="custom-select" id="country">
+          {countries.map((item) => {
+            return (
+              <option key={item} value={item}>
+                {item}
+              </option>
+            );
+          })}
+        </select>
+      </div>
+    );
+  }
+}
